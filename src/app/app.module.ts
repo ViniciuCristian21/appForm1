32484a7c1acf0bbcf6a8eb2c63f31e70b4984a0a
Viniciu Cristian <git.viniciu@gmail.com>
2021-03-03 16:46:19 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -10,8 +11,15 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+
+  imports: [BrowserModule,
+      FormsModule,
+      IonicModule.forRoot(),
+      AppRoutingModule,],
+
+  providers: [{ provide: RouteReuseStrategy,
+     useClass: IonicRouteStrategy }],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../shared/cliente';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  cliente: Cliente;
+  constructor() { }
 
-  constructor() {}
+  ngOnInit() {
+    this.cliente = new Cliente();
+  }
 
+  onSubmit(){
+    console.log(this.cliente);
+  }
 }
